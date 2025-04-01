5f5a2ef91e301414d8fb23710e2560963719927b
Omar Aljazairy <onha2001@gmail.com>
2025-04-01 12:24:12 +0200

    added gluestack and one component

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, FlatList } from 'react-native'
+import React from 'react';
+import products from '../assets/products.json';
+import ProductListItem from '../components/ProductListItem';
+
 
 const index = () => {
   return (
-    <View>
-      <Text>index page v1</Text>
-    </View>
+    <FlatList
+        data={products}
+        renderItem={({item}) => (
+            ProductListItem({product: item})
+        )}
+    >
+    </FlatList>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+});
 
 export default index
